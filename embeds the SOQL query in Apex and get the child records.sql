059d3f61ff772946dc28c4e55f2042ec0f2f9336
Vuk Dukic <VukDukic@users.noreply.github.com>
2016-03-02 12:34:29 -0800

    Create embeds the SOQL query in Apex and get the child records.sql
    embeds the SOQL query in Apex and shows how to get the child records from the SOQL result by using the Contacts relationship name on the sObject.

---
+++ embeds the SOQL query in Apex and get the child records.sql
@@ -0,0 +1,5 @@
+Contact[] cts = [SELECT Account.Name FROM Contact 
+                 WHERE FirstName = 'Carol' AND LastName='Ruiz'];
+Contact carol = cts[0];
+String acctName = carol.Account.Name;
+System.debug('Carol\'s account name is ' + acctName);
