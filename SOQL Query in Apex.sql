0930cd9858921323e06cdcad2c1e41157c0b47dc
Vuk Dukic <VukDukic@users.noreply.github.com>
2016-03-02 10:46:17 -0800

    Update SOQL Query in Apex.sql

--- SOQL Query in Apex.sql
+++ SOQL Query in Apex.sql
@@ -6,3 +6,8 @@ System.debug(accts.size() + ' account(s) returned.');
 // Write all account array info
 System.debug(accts);
 
+USER_DEBUG [7]|DEBUG|(
+Account:{Name=SFDC Computing, Phone=(415)555-1212, Id=001F000001WonNqIAJ}, 
+Account:{Name=SFDC Computing, Phone=(415)555-1212, Id=001F000001WosIvIAJ}, 
+Account:{Name=SFDC Computing, Phone=(415)555-1212, Id=001F000001iEWzRIAW}
+)
